38a8cc27d9c9ee5367c62f5b85e4432f5337fb44
shurtado1916-cell <shurtado1916@gmail.com>
2026-05-28 12:10:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown("""
     /* Fondo total de la aplicación (Negro profundo mate) */
     .stApp {
         background-color: #070b19 !important;
-        color: #E0E0E0 !important;
+        color: #070b19 !important;
     }
     
     /* Títulos principales estilo deportivo (Verde Neón) */
